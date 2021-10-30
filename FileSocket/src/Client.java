bd4d0e8341a5bd8d38ad55f7a6a095f0a61ffda8
NeutronSun <lorenzosanseverino2003@gmail.com>
2021-10-30 12:46:55 +0200

    tier 1

--- FileSocket/src/Client.java
+++ FileSocket/src/Client.java
@@ -4,6 +4,10 @@ import java.net.*;
 import java.io.IOException;
 
 public class Client {
+    private static Socket echoSocket;
+    private static PrintWriter putInServer;
+    private static BufferedReader in;
+    private static BufferedReader read;
     public static void main(String[] args){
         //String hostName = Inet4Address.getLocalHost().getHostAddress();
         int portNumber = 77;
@@ -11,35 +15,50 @@ public class Client {
         try{
 
             String hostName = Inet4Address.getLocalHost().getHostAddress();
-            Socket echoSocket = new Socket(hostName, portNumber);
-            PrintWriter putInServer = new PrintWriter(echoSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            DataOutputStream dataOutputStream = new DataOutputStream(echoSocket.getOutputStream());
-            String userInput;
-            System.out.println("create a file||Choose the name");
-            String nameF = in.readLine();
-            File file = new File((nameF + ".txt"));
-            FileWriter writer = new FileWriter(file);
-            for(int i = 0; i < 100; i++){
-                writer.write(String.valueOf(i));
+            echoSocket = new Socket(hostName, portNumber);
+            putInServer = new PrintWriter(echoSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(System.in));
+            read = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+            String userInput, s, caseCase = "ez";
+            while(true){
+                System.out.println("select --> to select a file to download");
+                System.out.println("byebye --> to quit");
+                switch(caseCase = in.readLine()){
+                    case "select" : 
+                        //say to the server to send all the files name
+                        putInServer.println("sentToMe");
+                        //read all the name
+                        while(!(s = read.readLine()).equals("end")){System.out.println(s);};
+                        System.out.println(read.readLine());
+                        //choose the file
+                        String fileName = in.readLine();
+                        putInServer.println(fileName);
+                        //recive the file
+                        getFile(fileName);
+                        break;
+
+                    case "byebye":
+                        putInServer.close();
+                        in.close();
+                        echoSocket.close();
+
+                    default:
+                    System.out.println("wrong case");
+
+                }
             }
-            writer.flush();
-            byte[] buffer = new byte[1000*1024];
-            FileInputStream fileInputStream = new FileInputStream(file);
-            int bytes = fileInputStream.read(buffer,0,buffer.length);
-            dataOutputStream.write(buffer,0, bytes);        
-
-
-
-
-            
-            
-            putInServer.close();
-            in.close();
         } catch (IOException e) {
             System.out.println("eghiyt");
             return;
         }
-       
+    }
+
+    public static void getFile(String fileName) throws IOException{
+        byte[] buffer = new byte[1000*1024];
+        DataInputStream dataInputStream = new DataInputStream(echoSocket.getInputStream());
+        int bytes = dataInputStream.read(buffer,0,buffer.length);
+        FileOutputStream fileOutputStream = new FileOutputStream("./CarpetaClient/" + fileName);
+        fileOutputStream.write(buffer,0,bytes);
+        System.out.println("nothing went wrong");
     }
 }
--- FileSocket/src/Server.java
+++ FileSocket/src/Server.java
@@ -2,23 +2,59 @@ import java.net.*;
 import java.io.*;
 
 public class Server {
+
+    public static PrintWriter putInClient;
+    public static BufferedReader getFromItSelf;
+    public static DataOutputStream dataOutputStream;
     public static void main(String[] args) throws IOException {
         int portNumber = 77;
-        try (
+        try{
             ServerSocket serverSocket = new ServerSocket(portNumber);
             Socket clientSocket = serverSocket.accept();
 
-            PrintWriter putIncliet = new PrintWriter(clientSocket.getOutputStream(), true);                   
-            BufferedReader getFromItSelf = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            putInClient = new PrintWriter(clientSocket.getOutputStream(), true);                   
+            getFromItSelf = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
+            String caseCase = "wgui";
             String userInput;
-            byte[] buffer = new byte[1000*1024];
-            DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
-            int bytes = dataInputStream.read(buffer,0,buffer.length);
-            FileOutputStream fileOutputStream = new FileOutputStream("clientFile.txt");
-            fileOutputStream.write(buffer,0,bytes);
-            System.out.println("nothing went wrong");
+            while(true){
+                caseCase = getFromItSelf.readLine();
+                switch (caseCase){
+                    case "sentToMe":
+                    //send to the client all the file name
+                    listFilesForFolder(new File("./CarpetaServer"));
+                    putInClient.println("select a file");
+                    userInput = getFromItSelf.readLine();
+                    sendFile(userInput);
+                    break;
+
+                    case "byebye":
+                    return;
+                }
+            }
         } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port ");
         }
+    }
+
+    public static void listFilesForFolder(final File folder) {
+        int cont = 0;
+        for (final File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                listFilesForFolder(fileEntry);
+            } else {
+                putInClient.println("File " + cont + ":" + fileEntry.getName());
+                System.out.println(fileEntry.getName());
+                cont++;
+            }
+        }
+        putInClient.println("end");
+    }
+
+    public static void sendFile(String fineName) throws IOException{
+        byte[] buffer = new byte[1000*1024];
+        FileInputStream fileInputStream = new FileInputStream("./CarpetaServer/" + fineName);
+        int bytes = fileInputStream.read(buffer,0,buffer.length);
+        dataOutputStream.write(buffer,0, bytes); 
     }
 }
